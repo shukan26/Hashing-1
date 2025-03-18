7bc095ee8cec481f9b532d305fb98b0734bf8820
Shukan Shah <shukan@Mac.attlocal.net>
2025-03-17 22:55:53 -0700

    added comments

--- IsomorphicString.java
+++ IsomorphicString.java
@@ -1,5 +1,14 @@
 import java.util.*;
 
+/*
+ * Create a hashmap to store the mapping of characters from string s to string t.
+ * Create a hashset to store the characters of string t.
+ * Iterate through the string s and check if the character is present in the hashmap.
+ * If it is not present, check if the character is present in the hashset.
+ * If it is not present in the hashset, add the character to the hashmap and hashset.
+ * If it is present in the hashset, return false.
+ * If it is present in the hashmap, check if the character in the hashmap is equal to the character in the string t. If it is not equal, return false.
+ */
 
 public class IsomorphicString {
     public boolean isIsomorphic(String s, String t) {
